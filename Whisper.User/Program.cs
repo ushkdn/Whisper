3366f996bd276872d7785f565c8983bae77ad85f
ushkdn <ushkndn@gmail.com>
2025-04-28 19:56:35 +0300

    feat: setupped user microservice & compose

--- Whisper.User/Program.cs
+++ Whisper.User/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 namespace Whisper.User
 {
     public class Program
@@ -9,11 +12,37 @@ namespace Whisper.User
             builder.Services.AddControllers();
             builder.Services.AddOpenApi();
 
+            builder.Services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Whisper User API",
+                    Version = "v1",
+                    Description = "Whisper User API which provides base user operations",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "",
+                        Email = "",
+                        Url = new Uri("https://github.com/ushkdn/Whisper")
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                x.IncludeXmlComments(xmlPath);
+            });
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(x =>
+                {
+                    x.RoutePrefix = string.Empty;
+                    x.SwaggerEndpoint("/swagger/v1/swagger.json", string.Empty);
+                });
             }
 
             app.UseHttpsRedirection();
